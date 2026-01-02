3308c94c70d5967bfd66a6a3c1d1892b436a25ba
Ayananth <tsayananth93@gmail.com>
2026-01-02 14:01:02 +0530

    added groups

--- channels_demo/live/consumers.py
+++ channels_demo/live/consumers.py
@@ -4,10 +4,29 @@ counter = 0
 
 class CounterConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        await self.channel_layer.group_add(
+            "counter_group",
+            self.channel_name
+        )
         await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            "counter_group",
+            self.channel_name
+        )
 
     async def receive(self, text_data):
         global counter
-        if text_data == "increment":
-            counter += 1
-            await self.send(text_data=str(counter))
+        counter += 1
+
+        await self.channel_layer.group_send(
+            "counter_group",
+            {
+                "type": "broadcast_count",
+                "count": counter
+            }
+        )
+
+    async def broadcast_count(self, event):
+        await self.send(text_data=str(event["count"]))
